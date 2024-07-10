657c76486ebd6de186a31faf595d447ec109be6e
Amir Azam <114767866+amirazamweb@users.noreply.github.com>
2024-07-10 18:36:17 +0530

    Update orderMail.js

--- helpers/orderMail.js
+++ helpers/orderMail.js
@@ -28,7 +28,7 @@ today = mm + '/' + dd + '/' + yyyy;
     Sr. Software Engineer<br>
     Amir Store Pvt. Ltd<br>
     +91-9661272784<br>
-     <a href = 'http://localhost:3000/signup'>http://localhost:3000/</a>
+     <a href = 'https://amirstore.onrender.com/'>amirstore.onrender.com</a>
     `
 }
 
